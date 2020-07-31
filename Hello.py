2608ffbd48edc14fdded79e30516a7c8b5b46498
ranveersinghparihar <69052365+ranveersinghparihar@users.noreply.github.com>
2020-07-31 23:05:18 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,43 @@
+#basic print command
+print("hello world")
+#basic plus minus command
+print(3+2)
+#basic math module command
+import math
+print(math.gcd(3,6))
+
+
+# the code from 11-15 is about manupilating the no and letter types 
+a = 10
+b = 12
+c = print(str(a+b))
+c = (int(a + b))
+print(c + a)
+
+
+#question 1- make three types of data (string,float,integer) and then the random integer made needs to be converted into string and float?
+
+#answer--
+a = 10
+b = "10"
+c = 10.0
+a =(str(a))     #from line 24 to 
+print(type(a))
+print(a)
+a =(float(a))
+print(type(a)) 
+print(a) 
+        #line 29 is all for converting 'a'
+b=(int(b))    #from line 31 to
+print(type(b))
+print(b)
+b =(float(b))
+print(type(b))
+print(b)       #line 36 is for converting 'b' 
+
+c =(str(c))    #from line  38 to
+print(type(c))
+print(c)
+     #line 41 is for converting 'c'
+
+     
